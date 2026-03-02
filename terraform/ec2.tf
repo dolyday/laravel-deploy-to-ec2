b12489604fed915fe7d64066856a8159165bf925
dolyday <dolyday0@gmail.com>
2026-03-02 13:13:41 +0100

    Update and rename main.tf to ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "ec2_sg" {
+    name        = "ec2-sg"
+    description = "Security group for EC2 instance"
+    vpc_id      = data.aws_vpc.default.id
+
+    ingress {
+        description = "Allow HTTP"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "EC2 SG"
+    }
+}
+
+resource "aws_instance" "laravel_app_instance" {
+    ami           = "ami-042b4708b1d05f512"
+    instance_type = var.instance_type
+    key_name      = var.key_name
+
+    subnet_id                   = data.aws_subnet.default.id
+    associate_public_ip_address = true
+    vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+
+
+    tags = {
+        Name = "Deploy Laravel App to EC2"
+    }
+}
