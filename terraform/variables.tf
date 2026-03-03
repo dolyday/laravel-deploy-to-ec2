f35f33b09f3ea715247f593940f968c9a10cf4ef
dolyday <dolyday0@gmail.com>
2026-03-03 21:41:19 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,20 @@
 variable "key_name" {
-  default = "DevOpsKey"
+    default = "DevOpsKey"
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+    default = "t3.micro"
 }
 
 variable "region" {
-  default = "eu-north-1"
+    default = "eu-north-1"
 }
 
 variable "db_username" {
-  description = "اسم مستخدم قاعدة البيانات"
-  type        = string
+  type = string
 }
 
 variable "db_password" {
-  description = "كلمة مرور قاعدة البيانات"
   type        = string
   sensitive   = true
 }
